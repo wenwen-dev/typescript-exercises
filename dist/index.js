54783344def2d89a0ed1a14b9bacc4722ae8390e
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-06-17 10:45:40 -0700

    Learn data types tuple and enum

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,17 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let numbers = [1, 2, 3, '5'];
+let user = [1, 'Wenwen'];
+user.push('aha');
+const small = 1;
+const medium = 2;
+const large = 3;
+let mySize = 2;
+console.log(mySize);
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+function render(document) {
+    console.log(document);
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+let numbers = [1,2,3, '5'];
+
+let user: [number, string] = [1, 'Wenwen'];
+user.push('aha');
+
+const small = 1;
+const medium = 2;
+const large = 3;
+
+const enum Size { Small, Medium, Large } //default values: 0, 1, 2...
+
+const enum Size2 {Small = 1, Medium, Large} // 1, 2, 3
+const enum Size3 {Small = 's', Medium = 'm', Large = 'l' }
+
+let mySize: Size2 = Size2.Medium;
+
+console.log(mySize);
+
+
 // let sales: number = 123_456_789;
 let sales = 123_456_789;
 let course: string = 'TypeScript';
@@ -11,6 +30,5 @@ function render(document: any) {
   console.log(document);
 }
 
-let numbers = [1,2,3];
 
-numbers.forEach(n=>n.toPrecision)
+
