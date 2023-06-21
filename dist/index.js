7ff399ae1c8d862c3fc8c4d015e683eeda29a1c2
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-06-21 12:31:57 -0700

    Learn access modifiers

--- dist/index.js
+++ dist/index.js
@@ -3,16 +3,20 @@ class Account {
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error('invalid amount');
-        this.balance += amount;
+        this._balance += amount;
+    }
+    getBalance() {
+        return this._balance;
     }
 }
-let account = new Account(123, 'potato', 50);
+let account = new Account(123, 'potato', 60);
 account.deposit(50);
 console.log(typeof account);
 console.log(account instanceof Account);
+console.log(account.getBalance());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,42 @@
 class Account {
   readonly id: number;
   owner: string;
-  balance: number;
+  private _balance: number;
   nickname?: string;
 
   constructor(id: number, owner: string, balance: number) {
     this.id = id;
     this.owner = owner;
-    this.balance = balance;
+    this._balance = balance;
   }
 
   deposit(amount: number): void {
     if (amount <= 0)
       throw new Error('invalid amount');
-    this.balance += amount;
+    this._balance += amount;
+    this.calculateTax();
+  }
+
+  private calculateTax(){
 
   }
+
+  getBalance(): number {
+    return this._balance;
+  }
+
 }
 
-let account = new Account(123, 'potato', 50);
+let account = new Account(123, 'potato', 60);
 
 account.deposit(50);
 console.log(typeof account);
 console.log(account instanceof Account);
+
+
+console.log(account.getBalance());
+
+
+
 
 
