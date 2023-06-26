1ed641715752b0b7e9ae7ac67f096a0ee917d2f1
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-06-26 08:11:55 -0700

    Learn parameterised decorators

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
-function Component(constructor: Function) {
-  console.log('Component decorator called.');
-  constructor.prototype.uniqueId = Date.now();
-  constructor.prototype.insertInDOM = () => {
-    console.log('Inserting the component in the DOM'); 
+type ComponentOptions = {
+  selector: string;
+}
+
+
+
+function Component(options: ComponentOptions) {
+  return (constructor: Function) => {
+    console.log('Component decorator called.');
+    constructor.prototype.options = options;
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+      console.log('Inserting the component in the DOM'); 
+    }
   }
 }
-// parameter name can by anything, but type must be Function
-// no return type annotation?
 
-@Component
+
+@Component({ selector: '#my-profile'})
 class ProfileComponent {
 }
 
