a39d25978671f8e59f389e3d459c9eae233fd7ba
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-06-29 12:54:24 -0700

    Learn module format config

---
+++ dist/shapes.js
@@ -0,0 +1,11 @@
+export class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+}
+export class Square {
+    constructor(width) {
+        this.width = width;
+    }
+}
+//# sourceMappingURL=shapes.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,40 +4,39 @@ import { Circle } from "./shapes";
 
 let circle = new Circle(2);
 
+console.log(circle.radius);
 
 
+// import { Product } from './Product'
 
+// let product: Product = {name: 'ss'}
 
-
-
-
-
-type ComponentOptions = {
-  selector: string;
-}
-
-
-
-function Component(options: ComponentOptions) {
-  return (constructor: Function) => {
-    console.log('Component decorator called.');
-    constructor.prototype.options = options;
-    constructor.prototype.uniqueId = Date.now();
-    constructor.prototype.insertInDOM = () => {
-      console.log('Inserting the component in the DOM'); 
-    }
-  }
-}
-
-
-@Component({ selector: '#my-profile'})
-class ProfileComponent {
-}
-
-// class Component {
-//   insertInDOM() {}
-
+// type ComponentOptions = {
+//   selector: string;
 // }
 
-// class ProfileComponent extends Component{
+
+
+// function Component(options: ComponentOptions) {
+//   return (constructor: Function) => {
+//     console.log('Component decorator called.');
+//     constructor.prototype.options = options;
+//     constructor.prototype.uniqueId = Date.now();
+//     constructor.prototype.insertInDOM = () => {
+//       console.log('Inserting the component in the DOM'); 
+//     }
+//   }
 // }
+
+
+// @Component({ selector: '#my-profile'})
+// class ProfileComponent {
+// }
+
+// // class Component {
+// //   insertInDOM() {}
+
+// // }
+
+// // class ProfileComponent extends Component{
+// // }
